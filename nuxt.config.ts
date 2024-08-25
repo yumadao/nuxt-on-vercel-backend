caddf56ee9ebba6227545cdfada7842020741b08
yoishi <75917699+yumadao@users.noreply.github.com>
2024-08-25 23:15:33 +0900

    add nuxthub modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxthub/core"],
+  hub: {
+    database: true,
+    kv: true,
+    blob: true,
+    cache: true,
+  },
+});
